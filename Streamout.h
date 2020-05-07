e38b6940b9aa8f4872c308ecde2ec324fc611644
edward nelson <u3560357@connect.hku.hk>
2020-05-08 00:06:03 +0800

    fifteenth-commit

---
+++ Streamout.h
@@ -0,0 +1,12 @@
+#ifndef STREAMOUT_H
+#define STREAMOUT_H
+
+#include <string>
+#include "Node.h"
+
+using namespace std;
+
+void print_output(string outputfile, Node * head, char grid[][10+1]);
+void print_list(Node *head);
+
+#endif
