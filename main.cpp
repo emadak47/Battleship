e0b737e014cd5c91643242ad6556ec2902f385aa
Edward Nelson <63388347+edward-nelson@users.noreply.github.com>
2020-05-08 14:53:48 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,24 +16,28 @@
 using namespace std;
  
 int main() {
+// take and store name of first user
   string name1; 
   cout << "Please enter the name of Player 1: ";
   cin >> name1;
   P1.PlayerName = name1;
-
+	
+// take and store second user name
   string name2; 
   cout << "Please enter the name of Player 2: ";
   cin >> name2;
   P2.PlayerName = name2;
-
+//create grid for player 1 and player 2 by calling the initialise_ship_grid function
   Initialise_Ship_Grid(P1.ShipGrid);
   Initialise_Ship_Grid(P2.ShipGrid);
+
 
   read_input("inputfile1.txt");
   if (checkvalid == false) return 0;
   read_input("inputfile2.txt");
   if (checkvalid == false) return 0;
   
+//create their 'updated grid' to record all their next moves
   Initialise_Record_Grid(P1.RecordGrid);
   Initialise_Record_Grid(P2.RecordGrid);
 
@@ -43,15 +47,18 @@ int main() {
   Node * head1 = NULL, * tail1 = NULL;
   Node * head2 = NULL, * tail2 = NULL;
   int x, y; 
-	//remember to let the player have another go if he hits
+	//ends game when all ships of one player have been sunken.
   while (Win_Game(P1.ShipGrid) == false && Win_Game(P2.ShipGrid) == false)
   {
+// determines that it is player 1's turn
     if (starter % 2 == 1)
     {
       Print_Attack_Record(P1.RecordGrid);
       cout << P1.PlayerName << ", Enter coordinates for your next attack: ";
       cin >> x >> y;
-      if ( x < 1 || x > 10 || y < 1 || y > 10 ) cout << "Please think inside the box next time :)" << endl;
+//enusres that coordinate inputed is within the grid
+      if ( x < 1 || x > 10 || y < 1 || y > 10 ) cout << "Please think inside the box next time :)" << endl;\
+//ensures coordinate was not used before and updates the update grid with 'H' if hit and 'X' if miss
       if (Search_History(head1,x,y) == false)
       {
         Build_linked_list(head1, tail1, x, y);
@@ -110,12 +117,14 @@ int main() {
   }
 
   char choice;
+//if player 2 wins declares player 2 as the winner
   if (Win_Game(P2.ShipGrid)) 
   {
     cout << "Game Over " << P2.PlayerName << "!" << endl;
     cout << P1.PlayerName << " wins" << endl;
     cout << P1.PlayerName << ", would like to see a record of all your moves? (Y/N): ";
     cin >> choice;
+//optional output file.
     if (choice == 'Y')
     {
       cout << "Please open outputfile1 to see your record." << endl;
@@ -123,7 +132,7 @@ int main() {
     }
     else cout << "Bye!" << endl;
   }
-
+//if player 1 wins it declares player 1 as the winner.
   if (Win_Game(P1.ShipGrid)) 
   {
     cout << "Game Over " << P1.PlayerName << " !" << endl;
@@ -138,6 +147,7 @@ int main() {
     else cout << "Bye!" << endl;
   }
   
+//delete the linked lists 
   delete_list(head1);
   delete_list(head2);
 
